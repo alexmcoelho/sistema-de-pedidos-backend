8440ee5ad4f8909e94cc31c1cee5ca396599adf8
alexmcoelho <alexma_coelho@hotmail.com>
2019-01-13 18:18:31 -0200

    Subtotal e total do pedido

--- src/main/java/com/alex/cursospringudemy/domain/ItemPedido.java
+++ src/main/java/com/alex/cursospringudemy/domain/ItemPedido.java
@@ -72,6 +72,10 @@ public class ItemPedido implements Serializable {
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
+	
+	public Double getSubTotal() {
+		return (this.preco - this.desconto) * this.quantidade;
+	}
 
 	@Override
 	public int hashCode() {
