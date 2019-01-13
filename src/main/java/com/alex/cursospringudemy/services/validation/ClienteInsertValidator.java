581c57fbca87b8f3d25dd989479d3e368dcb224a
alexmcoelho <alexma_coelho@hotmail.com>
2019-01-13 14:54:48 -0200

    Validação de CPF e CNPJ personalizado

---
+++ src/main/java/com/alex/cursospringudemy/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,46 @@
+package com.alex.cursospringudemy.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.alex.cursospringudemy.domain.Cliente;
+import com.alex.cursospringudemy.domain.enums.TipoCliente;
+import com.alex.cursospringudemy.dto.ClienteNewDTO;
+import com.alex.cursospringudemy.resources.exception.FieldMessage;
+import com.alex.cursospringudemy.services.validation.util.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>(); // inclua os testes aqui, inserindo erros na lista
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		/*Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}*/
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFielName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
