59561f4dceacfa278db5b42b3d6f438970304f6b
alexmcoelho <alexma_coelho@hotmail.com>
2019-02-07 23:03:46 -0200

    Implementando autorização

--- src/main/java/com/alex/cursospringudemy/config/SecurityConfig.java
+++ src/main/java/com/alex/cursospringudemy/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alex.cursospringudemy.security.JWTAuthenticationFilter;
+import com.alex.cursospringudemy.security.JWTAuthorizationFilter;
 import com.alex.cursospringudemy.security.JWTUtil;
 import com.mysql.cj.x.protobuf.MysqlxSession.AuthenticateContinueOrBuilder;
 
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
